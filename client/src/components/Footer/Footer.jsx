427bd21084b5537ff86959523f77a01c5a772107
Sahan Perera <sahanvimukthi00@gmail.com>
2023-02-17 21:17:31 +0530

    did some changes related to advertisement carousel

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,11 @@
 import "./Footer.css";
+import {
+  UilFacebookF,
+  UilInstagram,
+  UilLinkedin,
+  UilTwitter,
+  UilWhatsapp,
+} from "@iconscout/react-unicons";
 
 export default function Footer() {
   return (
@@ -7,6 +14,25 @@ export default function Footer() {
         src="https://firebasestorage.googleapis.com/v0/b/vedra-8d493.appspot.com/o/W.png?alt=media&token=87a8af90-8889-4e84-9553-bc22b39b1d62"
         alt=""
       />
+      <div className="footerBottomCont">
+        <ul>
+          <li>
+            <UilFacebookF />
+          </li>
+          <li>
+            <UilInstagram />
+          </li>
+          <li>
+            <UilLinkedin />
+          </li>
+          <li>
+            <UilTwitter />
+          </li>
+          <li>
+            <UilWhatsapp />
+          </li>
+        </ul>
+      </div>
       <span>
         Vedra.com © Copyright 1997-2023 Vedra, LLC. All rights reserved. Vedra®
         is a registered trademark of Vedra, LLC. Trusted Brands. Healthy
