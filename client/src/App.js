50ba81bf2804cef4fb3eba356ab1b13bef2fd550
Sahan Perera <sahanvimukthi00@gmail.com>
2023-02-17 21:30:32 +0530

    react router dom installed and put the routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
