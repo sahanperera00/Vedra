6af32430dbad3ad37a1f533dcffb1ddc780af3d6
Sahan Perera <sahanvimukthi00@gmail.com>
2023-02-17 13:08:44 +0530

    home page created

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import "./Home.css";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+
+export default function Home() {
+  return (
+    <div className="home">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
