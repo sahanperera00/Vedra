8676b1b0a32d65193bd8df417c95b22b90214d6a
Sahan Perera <sahanvimukthi00@gmail.com>
2023-02-18 11:54:47 +0530

    product page created

---
+++ client/src/pages/Product/Product.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Navbar/Navbar";
+import "./Product.css";
+
+export default function Product() {
+  return (
+    <div className="product">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
