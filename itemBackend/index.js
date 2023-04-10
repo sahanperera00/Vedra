73de731b2e8e031b36a15e255b66977cffce7b73
Sahan Perera <sahanvimukthi00@gmail.com>
2023-04-10 18:48:57 +0530

    item router added to the server

--- itemBackend/index.js
+++ itemBackend/index.js
@@ -3,12 +3,15 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv/config";
+import itemRoutes from "./routes/item.js";
 
 const app = express();
 const PORT = process.env.PORT || 8081;
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use("/items", itemRoutes);
 
 const URL = process.env.MONGODB_URL;
 mongoose.connect(URL);
