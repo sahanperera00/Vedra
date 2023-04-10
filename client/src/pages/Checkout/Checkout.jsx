396757dee645363ed93ae3953b4d606202558e6a
Sahan Perera <sahanvimukthi00@gmail.com>
2023-04-11 05:15:21 +0530

    styles added

--- client/src/pages/Checkout/Checkout.jsx
+++ client/src/pages/Checkout/Checkout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
+import Footer from "../../components/Footer/Footer";
 
 export default function Checkout() {
   return (
@@ -8,32 +9,19 @@ export default function Checkout() {
       <Navbar />
       <div class="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
         <a href="#" class="text-2xl font-bold text-gray-800">
-          sneekpeeks
+          Shipping Details
         </a>
-        <div class="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base">
+        <div class="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base px-[100px]">
           <div class="relative">
             <ul class="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
               <li class="flex items-center space-x-3 text-left sm:space-x-4">
-                <a
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-emerald-200 text-xs font-semibold text-emerald-700"
-                  href="#"
+                <Link
+                  to="/cart"
+                  class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-4 w-4"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M5 13l4 4L19 7"
-                    />
-                  </svg>
-                </a>
-                <span class="font-semibold text-gray-900">Shop</span>
+                  1
+                </Link>
+                <span class="font-semibold text-gray-900">Cart</span>
               </li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -50,42 +38,19 @@ export default function Checkout() {
                 />
               </svg>
               <li class="flex items-center space-x-3 text-left sm:space-x-4">
-                <a
+                <Link
+                  to="/checkout"
                   class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-600 text-xs font-semibold text-white ring ring-gray-600 ring-offset-2"
-                  href="#"
                 >
                   2
-                </a>
+                </Link>
                 <span class="font-semibold text-gray-900">Shipping</span>
-              </li>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                stroke-width="2"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9 5l7 7-7 7"
-                />
-              </svg>
-              <li class="flex items-center space-x-3 text-left sm:space-x-4">
-                <a
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
-                  href="#"
-                >
-                  3
-                </a>
-                <span class="font-semibold text-gray-500">Payment</span>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+      <div class="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32 mb-[50px]">
         <div class="px-4 pt-8">
           <p class="text-xl font-medium">Order Summary</p>
           <p class="text-gray-400">
@@ -342,6 +307,7 @@ export default function Checkout() {
           </Link>
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
