1ec2745b5ef21a84477b19216cbc49eb49823499
NashaliP <nashaliperera0@gmail.com>
2023-04-13 00:58:35 +0530

    small changes

--- userBackend/index.js
+++ userBackend/index.js
@@ -1,14 +1,16 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
-import dotenv from "dotenv/config";
 import userRouter from "./routes/user.js";
 
 const app = express();
 const PORT = process.env.PORT || 8084;
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use("/user", userRouter);
