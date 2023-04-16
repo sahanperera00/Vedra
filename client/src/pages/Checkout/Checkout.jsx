3a53ea0e61163f2a6ce7a82da0040f1b558f6cb2
DevinduSamarasinghe <devindu990@gmail.com>
2023-04-16 17:27:42 +0530

    +useState changes

--- client/src/pages/Checkout/Checkout.jsx
+++ client/src/pages/Checkout/Checkout.jsx
@@ -4,212 +4,240 @@ import { Link } from "react-router-dom";
 import Footer from "../../components/Footer/Footer";
 import axios from "axios";
 
+
 //importing stripe checkout
 import StripeCheckout from "react-stripe-checkout";
 
 export default function Checkout() {
 
+  const [order, setOrder] = useState({});  //sets order details to this state
+  const [orderItems, setOrderItems] = useState([]); //retrieving the order Items
+
+  //payment parameters to pass 
+
+  const [invoiceNo,setInvoiceNo] = useState("");
+  const [orderNo,setOrderNo] = useState("");
+  const [pmtDate, setPmtDate] = useState("");
+  const [email,setEmail] = useState("");
+  const [grossPrice,setGrossPrice] = useState(0);
+  const [shipping,setShipping] = useState(0);
+  const [netPrice,setNetPrice] = useState(0);
+  
+  //publishable key for Vedran's Stripe account
   const pmtKey = "pk_test_51MwfKHDtOg3Q5sN3OTX8k5fywchFMqv9sy758Q8M8hXDpucAadXrkdN33IluVD0eeaf8bNEt0jzxXH0OVRBbqYo400lE3qUaIP";
 
-
+  //Dummy passs
   const handlePmtToken = async (token, address) => {
-    const response = await axios.post("http://localhost:8082/pay", { token, product1 });
-
+    const response = await axios.post("http://localhost:8082/payment/pay", { token, order });
     console.log(response.status);
   }
 
-  const product1 = {
-    name: "Nike Air Max Pro - Super Light",
-    size: "42EU - 8.5US",
-    price: 138.99
+  //retrieving the order Content
+  const getOrder = async () => {
+    try {
+      const orderId = '6438fa2c518a57cbd5bdc8f4';
+      await axios.get(`http://localhost:8083/orders/${orderId}`)
+        .then((res) => {
+          setOrder(res.data);
+          setOrderItems(res.data.items);
+
+          console.log(order); //order details 
+          console.log(orderItems);  //order Items
+        }).catch((err) => {
+          console.log(err);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   }
-  const product2 = {
-    name: "Adidas Parley Hi Rez - Super Light",
-    size: "42EU - 8.5US",
-    price: 130.99
-  }
+
+  useEffect(() => {
+    getOrder();
+  }, [])
+
+  useEffect(()=>{
+    setGrossPrice(parseFloat(order.total));
+  })
+
+  useEffect(()=>{
+    setNetPrice(grossPrice + shipping);
+  })
 
 
-  const total = product1.price + product2.price;
 
+ 
+
+  //creating a payment in backend 
+ // const [payment, setPayment] = useState({});
   return (
     <div>
       <Navbar />
-      <div class="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
-        <a href="#" class="text-2xl font-bold text-gray-800">
+      <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+        <a href="#" className="text-2xl font-bold text-gray-800">
           Checkout
         </a>
-        <div class="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base px-[100px]">
-          <div class="relative">
-            <ul class="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
-              <li class="flex items-center space-x-3 text-left sm:space-x-4">
+        <div className="mt-4 py-2 text-xs sm:mt-0 sm:ml-auto sm:text-base px-[100px]">
+          <div className="relative">
+            <ul className="relative flex w-full items-center justify-between space-x-2 sm:space-x-4">
+              <li className="flex items-center space-x-3 text-left sm:space-x-4">
                 <Link
                   to="/cart"
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
+                  className="flex h-6 w-6 items-center justify-center rounded-full bg-gray-400 text-xs font-semibold text-white"
                 >
                   1
                 </Link>
-                <span class="font-semibold text-gray-900">Cart</span>
+                <span className="font-semibold text-gray-900">Cart</span>
               </li>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                class="h-4 w-4 text-gray-400"
+                className="h-4 w-4 text-gray-400"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
-                stroke-width="2"
+                strokeWidth="2"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M9 5l7 7-7 7"
                 />
               </svg>
-              <li class="flex items-center space-x-3 text-left sm:space-x-4">
+              <li className="flex items-center space-x-3 text-left sm:space-x-4">
                 <Link
                   to="/checkout"
-                  class="flex h-6 w-6 items-center justify-center rounded-full bg-[#278a9e] text-xs font-semibold text-white ring ring-[#278a9e] ring-offset-2"
+                  className="flex h-6 w-6 items-center justify-center rounded-full bg-[#278a9e] text-xs font-semibold text-white ring ring-[#278a9e] ring-offset-2"
                 >
                   2
                 </Link>
-                <span class="font-semibold text-gray-900">Checkout</span>
+                <span className="font-semibold text-gray-900">Checkout</span>
               </li>
             </ul>
           </div>
         </div>
       </div>
-      <div class="grid sm:px-10 lg:px-20 xl:px-32 mb-[100px]">
-        <div class="px-4 pt-8">
-          <p class="text-2xl font-bold">Order Summary</p>
-          <div class="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6">
-            <div class="flex flex-col rounded-lg bg-white sm:flex-row">
-              <img
-                class="m-2 h-24 w-28 rounded-md border object-cover object-center"
-                src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                alt=""
-              />
-              <div class="flex w-full flex-col px-4 py-4">
-                <span class="font-semibold">
-                  Nike Air Max Pro 8888 - Super Light
-                </span>
-                <span class="float-right text-gray-400">42EU - 8.5US</span>
-                <p class="text-lg font-bold">$138.99</p>
+      <div className="grid sm:px-10 lg:px-20 xl:px-32 mb-[100px]">
+        <div className="px-4 pt-8">
+          <p className="text-2xl font-bold">Order Summary</p>
+          {orderItems.map((data, key) => {
+            return (
+              <div className="mt-8 space-y-3 rounded-lg border bg-white px-2 py-4 sm:px-6" key={key}>
+                <div className="flex flex-col rounded-lg bg-white sm:flex-row">
+                  <img
+                    className="m-2 h-24 w-28 rounded-md border object-cover object-center"
+                    src={data.image}
+                    alt={data.name}
+                  />
+                  <div className="flex w-full flex-col px-4 py-4">
+                    <span className="font-semibold">{data.name}
+                    </span>
+                    <span className="float-right text-gray-400">Quantity {data.quantity}</span>
+                    <p className="text-lg font-bold">${data.quantity*data.price}</p>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div class="flex flex-col rounded-lg bg-white sm:flex-row">
-              <img
-                class="m-2 h-24 w-28 rounded-md border object-cover object-center"
-                src="https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-                alt=""
-              />
-              <div class="flex w-full flex-col px-4 py-4">
-                <span class="font-semibold">
-                  {product2.name}
-                </span>
-                <span class="float-right text-gray-400">{product2.size}</span>
-                <p class="mt-auto text-lg font-bold">{`$${product2.price}`}</p>
-              </div>
-            </div>
-          </div>
-
-          <p class="mt-8 text-2xl font-bold">Shipping Methods</p>
-          <form class="mt-5 grid gap-6">
-            <div class="relative">
+            )
+          })}
+          <p className="mt-8 text-2xl font-bold">Shipping Methods</p>
+          <form className="mt-5 grid gap-6">
+            <div className="relative">
               <input
-                class="peer hidden"
+                className="peer hidden"
                 id="radio_1"
                 type="radio"
                 name="radio"
-
+                value={grossPrice * 0.30}
+                onChange={(e)=>setShipping(parseFloat(e.target.value))}
               />
 
-              <span class="peer-checked:border-[#278a9e] absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <span className="peer-checked:border-[#278a9e] absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
               <label
-                class="peer-checked:border-2 peer-checked:border-[#278a9e] peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                for="radio_1"
+                className="peer-checked:border-2 peer-checked:border-[#278a9e] peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                htmlFor="radio_1"
               >
-                <img
-                  class="w-14 object-contain"
+                <img 
+                  className="w-14 object-contain"
                   src="/images/naorrAeygcJzX0SyNI4Y0.png"
                   alt=""
                 />
-                <div class="ml-5">
-                  <span class="mt-2 font-semibold">Fedex Delivery</span>
-                  <p class="text-slate-500 text-sm leading-6">
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold" >Fedex Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6" >
                     Delivery: 2-4 Days
                   </p>
                   <p>
-                    {`$ ${(total * .07).toFixed(2)}`}
+                  ${(grossPrice * .30).toFixed(2)}
                   </p>
                 </div>
               </label>
             </div>
-            <div class="relative">
+            <div className="relative">
               <input
-                class="peer hidden"
+                className="peer hidden"
                 id="radio_2"
                 type="radio"
                 name="radio"
+                value={grossPrice * 0.15}
+                onChange={(e)=>setShipping(parseFloat(e.target.value))}
               />
-              <span class="peer-checked:border-[#278a9e] absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <span className="peer-checked:border-[#278a9e] absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
               <label
-                class="peer-checked:border-2 peer-checked:border-[#278a9e] peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
-                for="radio_2"
+                className="peer-checked:border-2 peer-checked:border-[#278a9e] peer-checked:bg-gray-50 flex cursor-pointer select-none rounded-lg border border-gray-300 p-4"
+                htmlFor="radio_2"
               >
-                <img
-                  class="w-14 object-contain"
+                <img 
+                  className="w-14 object-contain"
                   src="/images/oG8xsl3xsOkwkMsrLGKM4.png"
                   alt=""
                 />
-                <div class="ml-5">
-                  <span class="mt-2 font-semibold">Standard Delivery</span>
-                  <p class="text-slate-500 text-sm leading-6">
+                <div className="ml-5" >
+                  <span className="mt-2 font-semibold"  >Standard Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
                     Delivery: 7 - 15 Days
+                    
                   </p>
                   <p>
-                    {`$ ${(total * .02).toFixed(2)}`}
+                  ${(grossPrice * .15).toFixed(2)}
                   </p>
                 </div>
               </label>
             </div>
           </form>
         </div>
-        <div class="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
-          <p class="text-2xl font-bold">Payment Details</p>
-          <div class="">
-            <div class="mt-6 border-t border-b py-2">
-              <div class="flex items-center justify-between">
-                <p class="text-sm font-medium text-gray-900">Subtotal</p>
-                <p class="font-semibold text-gray-900">{`$ ${total} `}</p>
+        <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-0">
+          <p className="text-2xl font-bold">Payment Details</p>
+          <div className="">
+            <div className="mt-6 border-t border-b py-2">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Subtotal</p>
+                <p className="font-semibold text-gray-900">{`$ ${(grossPrice).toFixed(2)} `}</p>
               </div>
-              <div class="flex items-center justify-between">
-                <p class="text-sm font-medium text-gray-900">Shipping</p>
-                <p class="font-semibold text-gray-900">$8.00</p>
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Shipping</p>
+                <p className="font-semibold text-gray-900">${(shipping).toFixed(2)}</p>
               </div>
             </div>
-            <div class="mt-6 flex items-center justify-between">
-              <p class="text-sm font-medium text-gray-900">Total</p>
-              <p class="text-2xl font-semibold text-gray-900">$408.00</p>
+            <div className="mt-6 flex items-center justify-between">
+              <p className="text-sm font-medium text-gray-900">Total</p>
+              <p className="text-2xl font-semibold text-gray-900">${(netPrice).toFixed(2)}</p>
             </div>
           </div>
           <div className="justify-center text-right">
-            
-              <button className="bg-[#3ea7ac] hover:bg-[#278a9e] text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-3 text-center mt-7"
-              onClick={()=>{
-                
-              }}>
-                Place Order
-             </button>
 
-             <StripeCheckout
-                className="bg-[#3ea7ac] hover:bg-[#278a9e] text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-3 text-center mt-7"
-                stripeKey={pmtKey}
-                token={handlePmtToken}
-                amount={total}
-                name={product1.name}
-                billingAddress
-                shippingAddress
-              ></StripeCheckout>
+            <button className="bg-[#3ea7ac] hover:bg-[#278a9e] text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-3 text-center mt-7">
+              Place Order
+            </button>
+
+            <StripeCheckout
+              className="bg-[#3ea7ac] hover:bg-[#278a9e] text-white focus:outline-none font-medium rounded-lg text-sm px-5 py-3 text-center mt-7"
+              stripeKey={pmtKey}
+              currency="USD"
+              token={handlePmtToken}
+              amount={order.total* 100}
+              name={order._id}
+              billingAddress
+              shippingAddress
+              
+            ></StripeCheckout>
           </div>
         </div>
       </div>
