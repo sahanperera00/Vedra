9ce6b88afdfc5889478bf8c575c4b73d85b4d8b1
DevinduSamarasinghe <devindu990@gmail.com>
2023-04-17 00:45:52 +0530

    +backend payment pass and stripe trigger

--- paymentBackend/routes/pmtRoutes.js
+++ paymentBackend/routes/pmtRoutes.js
@@ -1,10 +1,13 @@
 import { getFromUser,createPayment,getAllPayments, chargeUser } from "../controllers/pmtCtrl.js";
 import express from "express";
+import cors from "cors";
 
 const router = express.Router();
 
+router.options('*',cors())
 router.post("/create",createPayment);
 router.get("/:email",getFromUser);
+//router.options("/pay",cors());
 router.post("/pay",chargeUser);
 router.get("/",getAllPayments);
 
