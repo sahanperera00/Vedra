b5d68137689d243b287773a7157b2dece4e1dcae
NashaliP <nashaliperera0@gmail.com>
2023-04-17 16:13:04 +0530

    changed route url

--- userBackend/index.js
+++ userBackend/index.js
@@ -1,5 +1,4 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+import dotenv from "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -13,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
-app.use("/user", userRouter);
+app.use("/", userRouter);
 
 const URL = process.env.MONGODB_URL;
 mongoose.connect(URL);
