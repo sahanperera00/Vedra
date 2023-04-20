dca03427c77c4fadb84fa241499e0d5a43f836c1
Sahan Perera <85289620+sahanperera00@users.noreply.github.com>
2023-04-21 03:57:01 +0530

    validated the dashboadsssssssssssssssssssssss

--- client/src/pages/ClientDashboard/ClientDash.jsx
+++ client/src/pages/ClientDashboard/ClientDash.jsx
@@ -1,96 +1,109 @@
 import React, { useEffect, useState } from "react";
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import { useStateContext } from "../../contexts/ContextProvider";
-import { FiSettings } from 'react-icons/fi';
-import { Navbar, Footer, ThemeSettings } from "../../components/Tailwind/components";
-import ClientSidebar from "../../components/Tailwind/components/ClientSidebar"
+import { FiSettings } from "react-icons/fi";
+import {
+  Navbar,
+  Footer,
+  ThemeSettings,
+} from "../../components/Tailwind/components";
+import ClientSidebar from "../../components/Tailwind/components/ClientSidebar";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import jwtDecode from "jwt-decode";
 
 const ClientDash = () => {
-    const { 
-        setCurrentColor,
-        setCurrentMode,
-        currentMode,
-        activeMenu,
-        currentColor,
-        themeSettings,
-        setThemeSettings, } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
+  const navigate = useNavigate();
 
-        useEffect(() => {
-            const currentThemeColor = localStorage.getItem("colorMode"); // KEEP THESE LINES
-            const currentThemeMode = localStorage.getItem("themeMode");
-            if (currentThemeColor && currentThemeMode) {
-              setCurrentColor(currentThemeColor);
-              setCurrentMode(currentThemeMode);
-            }
-          }, []);
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("colorMode"); // KEEP THESE LINES
+    const currentThemeMode = localStorage.getItem("themeMode");
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
 
-    //retrieving payment information 
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      if (decodedToken.role === "seller" || decodedToken.role === "admin") {
+        navigate("/");
+      }
+    } else {
+      navigate("/signin");
+    }
+  }, []);
 
+  //retrieving payment information
 
-   
-    return (
-        <div>
+  return (
+    <div>
+      {/* DON'T CHANGE ANYTHING HERE */}
 
-            {/* DON'T CHANGE ANYTHING HERE */}
+      <div className={currentMode === "Dark" ? "dark" : ""}>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            {" "}
+            {/* THEME SETTINGS BUTTON */}
+            <TooltipComponent content="Settings" position="Top">
+              <button
+                type="button"
+                onClick={() => setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
 
-            <div className={currentMode === 'Dark' ? 'dark' : ''}>
-
-                <div className="flex relative dark:bg-main-dark-bg">
-
-                    <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}> {/* THEME SETTINGS BUTTON */}
-                        <TooltipComponent content="Settings" position="Top">
-                            <button
-                                type="button"
-                                onClick={() => setThemeSettings(true)}
-                                style={{ background: currentColor, borderRadius: '50%' }}
-                                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
-                            >
-                                <FiSettings />
-                            </button>
-                        </TooltipComponent>
-                    </div>
-
-
-                    {activeMenu ? ( // SIDEBAR IMPLEMENTATION
-                        <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
-                            <ClientSidebar />
-                        </div>
-                    ) : (
-                        <div className="w-0 dark:bg-secondary-dark-bg">
-
-                        </div>
-                    )}
-
-                    <div
-                        className={ // MAIN BACKGROUND IMPLEMENTATION
-                            activeMenu
-                                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
-                        }
-                    >
-
-                        {/* NAVBAR IMPLEMENTATION */}
-                        <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg w-full ">
-                            <Navbar />
-                        </div>
-
-                        <div>
-                            {themeSettings && <ThemeSettings />}
-                            <div>
-                                {/* Paste your content Here */}
-
-                                <h1 className="dark:text-gray-200 ml-5">Hello My Name is Billy Balloon Head</h1>
-                            </div>
-                            <Footer />
-                        </div>
-                    </div>
-                </div>
+          {activeMenu ? ( // SIDEBAR IMPLEMENTATION
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+              <ClientSidebar />
             </div>
+          ) : (
+            <div className="w-0 dark:bg-secondary-dark-bg"></div>
+          )}
+
+          <div
+            className={
+              // MAIN BACKGROUND IMPLEMENTATION
+              activeMenu
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
+            }
+          >
+            {/* NAVBAR IMPLEMENTATION */}
+            <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg w-full ">
+              <Navbar />
+            </div>
+
+            <div>
+              {themeSettings && <ThemeSettings />}
+              <div>
+                {/* Paste your content Here */}
+
+                <h1 className="dark:text-gray-200 ml-5">
+                  Hello My Name is Billy Balloon Head
+                </h1>
+              </div>
+              <Footer />
+            </div>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default ClientDash;
