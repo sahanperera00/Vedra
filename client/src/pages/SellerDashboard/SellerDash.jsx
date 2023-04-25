2ebc2528b49eaf30890c7086a5bf315148e5b23c
DevinduSamarasinghe <devindu990@gmail.com>
2023-04-25 13:22:15 +0530

    +seller dashboard

--- client/src/pages/SellerDashboard/SellerDash.jsx
+++ client/src/pages/SellerDashboard/SellerDash.jsx
@@ -16,6 +16,7 @@ import {
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import SellerSidebar from "../../components/Tailwind/components/SellerSidebar.jsx";
 import jwtDecode from "jwt-decode";
+import PaymentChart from "../../components/PaymentChart/PaymentChart.jsx";
 
 const SellerDash = () => {
   // <== THIS IS THE COMPONENT NAME, CHANGE IT TO YOUR COMPONENT NAME
@@ -170,78 +171,7 @@ const SellerDash = () => {
                           </div>
                           <div id="main-chart"></div>
                         </div>
-                        <div class="bg-white shadow rounded-lg p-4 sm:p-6 xl:p-8 ">
-                          <div class="mb-4 flex items-center justify-between">
-                            <div>
-                              <h3 class="text-xl font-bold text-gray-900 mb-2">
-                                Latest Transactions
-                              </h3>
-                              <span class="text-base font-normal text-gray-500">
-                                This is a list of latest transactions
-                              </span>
-                            </div>
-                            <div class="flex-shrink-0">
-                              <a
-                                href="/sellerPay"
-                                class="text-sm font-medium text-cyan-600 hover:bg-gray-100 rounded-lg p-2"
-                              >
-                                View all
-                              </a>
-                            </div>
-                          </div>
-                          <div class="flex flex-col mt-8">
-                            <div class="overflow-x-auto rounded-lg">
-                              <div class="align-middle inline-block min-w-full">
-                                <div class="shadow overflow-hidden sm:rounded-lg">
-                                  <table class="min-w-full divide-y divide-gray-200">
-                                    <thead class="bg-gray-50">
-                                      <tr>
-                                        <th
-                                          scope="col"
-                                          class="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Transaction
-                                        </th>
-                                        <th
-                                          scope="col"
-                                          class="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Date & Time
-                                        </th>
-                                        <th
-                                          scope="col"
-                                          class="p-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                                        >
-                                          Amount
-                                        </th>
-                                      </tr>
-                                    </thead>
-                                    <tbody class="bg-white">
-                                      {orders.map((order) => {
-                                        return (
-                                          <tr>
-                                            <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-900">
-                                              Payment from 
-                                              <span class="font-semibold">
-                                              {order.email}
-                                              </span>
-                                            </td>
-                                            <td class="p-4 whitespace-nowrap text-sm font-normal text-gray-500">
-                                              Apr 23 ,2021
-                                            </td>
-                                            <td class=" whitespace-nowrap text-sm font-semibold text-gray-900">
-                                              $ {order.total}
-                                            </td>
-                                          </tr>
-                                        );
-                                      })}
-                                    </tbody>
-                                  </table>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
+                          <PaymentChart orders={orders}/>
                       </div>
                     </div>
                   </div>
