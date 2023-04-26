2e147395de58b6e4ecf9c7c88d5181089194e3d0
DevinduSamarasinghe <devindu990@gmail.com>
2023-04-26 19:07:00 +0530

    +client order color dynamic color palette

--- client/src/pages/ClientDashboard/ClientOrders.jsx
+++ client/src/pages/ClientDashboard/ClientOrders.jsx
@@ -1,29 +1,42 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useStateContext } from '../../contexts/ContextProvider.js';
-import TableData from '../../components/Tailwind/components/Table/TableData.jsx';
-import TableHeader from '../../components/Tailwind/components/Table/TableHeader.jsx';
-import { FiSettings } from 'react-icons/fi';
-import { Header, Navbar, Footer, ThemeSettings } from '../../components/Tailwind/components';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
-import ClientSideBar from '../../components/Tailwind/components/ClientSidebar.jsx'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useStateContext } from "../../contexts/ContextProvider.js";
+import TableData from "../../components/Tailwind/components/Table/TableData.jsx";
+import TableHeader from "../../components/Tailwind/components/Table/TableHeader.jsx";
+import { FiSettings } from "react-icons/fi";
+import {
+  Header,
+  Navbar,
+  Footer,
+  ThemeSettings,
+} from "../../components/Tailwind/components";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import ClientSideBar from "../../components/Tailwind/components/ClientSidebar.jsx";
 
-import axios from 'axios';
+import axios from "axios";
 
-import { ToastContainer,toast,Zoom,Bounce } from 'react-toastify'
+import { ToastContainer, toast, Zoom, Bounce } from "react-toastify";
 
 /* IMPORT ALL YOUR IMPORTS AS USUAL ABOVE HERE, REMOVE UNNECESSARY ONES*/
 
 const ClientOrders = () => {
-
-
-  const { setCurrentColor, setCurrentMode, currentMode, activeMenu, currentColor, themeSettings, setThemeSettings, } = useStateContext();
+  const {
+    setCurrentColor,
+    setCurrentMode,
+    currentMode,
+    activeMenu,
+    currentColor,
+    themeSettings,
+    setThemeSettings,
+  } = useStateContext();
 
   const [orders, setOrders] = useState([]);
 
   const getOrders = async () => {
     await axios
-      .get(`http://localhost:8083/orders/email/${localStorage.getItem("email")}`)
+      .get(
+        `http://localhost:8083/orders/email/${localStorage.getItem("email")}`
+      )
       .then((res) => {
         console.log(res.data);
         setOrders(res.data);
@@ -35,12 +48,11 @@ const ClientOrders = () => {
 
   useEffect(() => {
     getOrders();
-  }, [])
+  }, []);
 
   useEffect(() => {
-
-    const currentThemeColor = localStorage.getItem('colorMode'); // KEEP THESE LINES
-    const currentThemeMode = localStorage.getItem('themeMode');
+    const currentThemeColor = localStorage.getItem("colorMode"); // KEEP THESE LINES
+    const currentThemeMode = localStorage.getItem("themeMode");
     if (currentThemeColor && currentThemeMode) {
       setCurrentColor(currentThemeColor);
       setCurrentMode(currentThemeMode);
@@ -48,67 +60,59 @@ const ClientOrders = () => {
   }, []);
 
   //status change
-  const orderStatus = async(id,stat)=>{
-    try{
-
+  const orderStatus = async (id, stat) => {
+    try {
       let status = null;
-      if( stat === "Reject"){
-         status = "Refunded";
-        
-      }else if(stat === "Approve"){
-         status = "Dispatched";
+      if (stat === "Reject") {
+        status = "Refunded";
+      } else if (stat === "Approve") {
+        status = "Dispatched";
       }
-      await axios.patch(`http://localhost:8083/orders/updateStatus`,{id,status})
-      .then((res)=>{
-        console.log(res.data);
-        console.log("order Status Updated");
-        if(status == "Dispatched"){
-          toast.success("Order Successfully Dispatched!");
-        }else{
-          toast.warn("Order Rejected!");
-        }
-        
-        
-      }) 
-    }catch(error){
+      await axios
+        .patch(`http://localhost:8083/orders/updateStatus`, { id, status })
+        .then((res) => {
+          console.log(res.data);
+          console.log("order Status Updated");
+          if (status == "Dispatched") {
+            toast.success("Order Successfully Dispatched!");
+          } else {
+            toast.warn("Order Rejected!");
+          }
+        });
+    } catch (error) {
       console.log(error);
     }
-  }
-
-
+  };
 
   //using the formatter
-  const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
+  const formatter = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
     minimumFractionDigits: 2,
-    currencyDisplay: 'symbol'
-  })
-
-
+    currencyDisplay: "symbol",
+  });
 
   return (
     <div>
-
       {/* DON'T CHANGE ANYTHING HERE */}
 
-      <div className={currentMode === 'Dark' ? 'dark' : ''}>
-        <ToastContainer/>
+      <div className={currentMode === "Dark" ? "dark" : ""}>
+        <ToastContainer />
         <div className="flex relative dark:bg-main-dark-bg">
-
-          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}> {/* THEME SETTINGS BUTTON */}
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            {" "}
+            {/* THEME SETTINGS BUTTON */}
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
                 onClick={() => setThemeSettings(true)}
-                style={{ background: currentColor, borderRadius: '50%' }}
+                style={{ background: currentColor, borderRadius: "50%" }}
                 className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
               >
                 <FiSettings />
               </button>
             </TooltipComponent>
           </div>
-
 
           {activeMenu ? ( // SIDEBAR IMPLEMENTATION
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
@@ -121,13 +125,13 @@ const ClientOrders = () => {
           )}
 
           <div
-            className={ // MAIN BACKGROUND IMPLEMENTATION
+            className={
+              // MAIN BACKGROUND IMPLEMENTATION
               activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  '
-                : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 '
+                ? "dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full  "
+                : "bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2 "
             }
           >
-
             {/* NAVBAR IMPLEMENTATION */}
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg  w-full ">
               <Navbar />
@@ -145,7 +149,7 @@ const ClientOrders = () => {
 
                     <div className=" flex items-center mb-5 "></div>
                     <div className="block w-full overflow-x-auto rounded-lg">
-                      <table className="w-full rounded-lg" >
+                      <table className="w-full rounded-lg">
                         <thead>
                           <tr className="bg-slate-200 text-md h-12 dark:bg-slate-800">
                             <TableHeader value="Order ID" />
@@ -157,27 +161,48 @@ const ClientOrders = () => {
                           </tr>
                         </thead>
                         <tbody>
-
                           {orders.map((data) => {
-                            if (data.status !== "cart" ) {
+                            if (data.status !== "cart") {
+                              let datacolor = "text-black ";
+                              if (data.status === "Pending") {
+                                datacolor = "text-orange-800 font-bold font-bold dark:text-orange-400";
+                              } else if(data.status === "Confirmed") {
+                                datacolor = "text-blue-800 font-bold font-bold dark:text-blue-400";
+                              } else if(data.status === "Dispatched") {
+                                datacolor = "text-green-700 font-bold"
+                              } else if(data.status === "Refunded") {
+                                datacolor = "text-red-700 font-bold"
+                              }
                               return (
-                                  <tr className="text-sm h-10 border dark:border-slate-600">
-                                    <TableData value={data._id} />
-                                    <TableData value={data.email} />
-                                    <TableData value={formatter.format(data.total)} />
-                                    <TableData value={formatter.format(data.total*.15)} />
+                                <tr className="text-sm h-10 border dark:border-slate-600">
+                                  <TableData value={data._id} />
+                                  <TableData value={data.email} />
+                                  <TableData
+                                    value={formatter.format(data.total)}
+                                  />
+                                  <TableData
+                                    value={formatter.format(data.total * 0.15)}
+                                  />
+                                  <td
+                                    className={`${datacolor} text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3`}
+                                  >
                                     <TableData value={data.status} />
-                                    <td className="text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
-                                        <Link to={`/orders/${data._id}`}>
-                                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">View Order</button>
-                                        </Link>
-                                    </td>
-                                  </tr>
-                              )
+                                  </td>
+                                  <td className="text-center px-3 align-middle border-l-0 border-r-0 text-m whitespace-nowrap p-3">
+                                    <Link to={`/orders/${data._id}`}>
+                                      <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full">
+                                        View Order
+                                      </button>
+                                    </Link>
+                                  </td>
+                                </tr>
+                              );
                             }
                           })}
                         </tbody>
-                      </table><br></br><br></br>
+                      </table>
+                      <br></br>
+                      <br></br>
                     </div>
                   </div>
                 </div>
