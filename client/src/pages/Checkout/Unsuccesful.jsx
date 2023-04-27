7338d1d4942927ae2a1082ba0e35bb8338de531b
ghostfacegangsta <chanukya.meshini.18@gmail.com>
2023-04-27 11:42:24 +0530

    readability check✔

--- client/src/pages/Checkout/Unsuccesful.jsx
+++ client/src/pages/Checkout/Unsuccesful.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ToastContainer,toast,Zoom,Bounce } from 'react-toastify'
-
+import { ToastContainer, toast, Zoom, Bounce } from "react-toastify";
 
 const Unsuccesful = () => {
-  toast.error("Payment Failed. Please try again");
+  toast.error("Payment Failed. Please try again"); //toast message
   return (
     <div class="lg:px-24 lg:py-24 md:py-20 md:px-44 px-4 py-24 items-center flex justify-center flex-col-reverse lg:flex-row md:gap-28 gap-16">
-      <ToastContainer/>
+      <ToastContainer />
       <div class="xl:pt-24 w-full xl:w-1/2 relative pb-12 lg:pb-0">
         <div class="relative">
           <div class="absolute">
