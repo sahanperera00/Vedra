f1cf85612f591cfcc38865a19c1849d9d3284deb
Sahan Perera <85289620+sahanperera00@users.noreply.github.com>
2023-05-12 16:52:38 +0530

    minor change on user BE

--- userBackend/index.js
+++ userBackend/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/", userRouter);
+app.use("/users", userRouter);
 
 const URL = process.env.MONGODB_URL;
 
